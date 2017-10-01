e30ee14cc8814adc8d8b90f9648e55e97edacebd
KoEonYack <21300035@handong.edu>
2017-10-01 12:33:21 +0900

    파이썬 lecture url수정

--- lecture/urls.py
+++ lecture/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^python_list/$', views.python, name='python_lecture'),
-    url(r'^python_list/python_1$', views.python1, name='python1'),
-    url(r'^python_list/python_2$', views.python2, name='python2'),
-    url(r'^python_list/python_3$', views.python2, name='python3'),
-    url(r'^python_list/python_4$', views.python2, name='python4'),
-    url(r'^python_list/python_5$', views.python2, name='python5'),
-    url(r'^python_list/python_6$', views.python2, name='python6'),
-    url(r'^python_list/python_7$', views.python2, name='python7'),
+    url(r'^python_list/$', views.python_lecture_list, name='python_lecture'),
+    url(r'^python_list/python_1$', views.python_lecture_1, name='python1'),
+    url(r'^python_list/python_2$', views.python_lecture_2, name='python2'),
+    url(r'^python_list/python_3$', views.python_lecture_3, name='python3'),
+    url(r'^python_list/python_4$', views.python_lecture_4, name='python4'),
+    url(r'^python_list/python_5$', views.python_lecture_5, name='python5'),
+    url(r'^python_list/python_6$', views.python_lecture_6, name='python6'),
+    url(r'^python_list/python_7$', views.python_lecture_7, name='python7'),
 ]
--- lecture/views.py
+++ lecture/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 
-def python(request):
+def python_lecture_list(request):
     return render(request, 'python/python_list.html')
 
-def python1(request):
+def python_lecture_1(request):
     return render(request, 'python/python_1.html')
 
-def python2(request):
+def python_lecture_2(request):
     return render(request, 'python/python_2.html')
 
-def python3(request):
+def python_lecture_3(request):
     return render(request, 'python/python_3.html')
 
-def python4(request):
+def python_lecture_4(request):
     return render(request, 'python/python_4.html')
 
-def python5(request):
+def python_lecture_5(request):
     return render(request, 'python/python_5.html')
 
-def python6(request):
+def python_lecture_6(request):
     return render(request, 'python/python_6.html')
 
-def python7(request):
+def python_lecture_7(request):
     return render(request, 'python/python_7.html')
 
