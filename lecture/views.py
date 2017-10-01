be24ce03df8d294f14f0d5c7960b9308c4035fa5
KoEonYack <21300035@handong.edu>
2017-10-01 12:46:32 +0900

    url test

--- lecture/views.py
+++ lecture/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 
 def python_lecture_list(request):
-    return render(request, 'python/python_list.html')
+    return render(request, 'python_list.html')
+# return render(request, 'python/python_list.html')
 
 def python_lecture_1(request):
-    return render(request, 'python/python_1.html')
+    return render(request, 'python_list/python_1.html')
 
 def python_lecture_2(request):
-    return render(request, 'python/python_2.html')
+    return render(request, 'python_list/python_2.html')
 
 def python_lecture_3(request):
-    return render(request, 'python/python_3.html')
+    return render(request, 'python_list/python_3.html')
 
 def python_lecture_4(request):
-    return render(request, 'python/python_4.html')
+    return render(request, 'python_list/python_4.html')
 
 def python_lecture_5(request):
-    return render(request, 'python/python_5.html')
+    return render(request, 'python_list/python_5.html')
 
 def python_lecture_6(request):
-    return render(request, 'python/python_6.html')
+    return render(request, 'python_list/python_6.html')
 
 def python_lecture_7(request):
-    return render(request, 'python/python_7.html')
+    return render(request, 'python_list/python_7.html')
 
