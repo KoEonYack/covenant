1297d98893d51075654855354b8efea926bc6873
KoEonYack <21300035@handong.edu>
2017-10-06 23:23:30 +0900

    url test2

--- lecture/views.py
+++ lecture/views.py
@@ -5,23 +5,23 @@ def python_lecture_list(request):
 # return render(request, 'python/python_list.html')
 
 def python_lecture_1(request):
-    return render(request, 'python_list/python_1.html')
+    return render(request, 'python/python_list/python_1.html')
 
 def python_lecture_2(request):
-    return render(request, 'python_list/python_2.html')
+    return render(request, 'python/python_list/python_2.html')
 
 def python_lecture_3(request):
-    return render(request, 'python_list/python_3.html')
+    return render(request, 'python/python_list/python_3.html')
 
 def python_lecture_4(request):
-    return render(request, 'python_list/python_4.html')
+    return render(request, 'python/python_list/python_4.html')
 
 def python_lecture_5(request):
-    return render(request, 'python_list/python_5.html')
+    return render(request, 'python/python_list/python_5.html')
 
 def python_lecture_6(request):
-    return render(request, 'python_list/python_6.html')
+    return render(request, 'python/python_list/python_6.html')
 
 def python_lecture_7(request):
-    return render(request, 'python_list/python_7.html')
+    return render(request, 'python/python_list/python_7.html')
 
